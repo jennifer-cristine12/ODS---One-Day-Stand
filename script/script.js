f3df9e457633c6d36f251f60bcb34cd356101c62
jennifer cristine <jennifer.cristine11@gmail.com>
2026-04-28 15:04:35 -0300

    estilo

--- script/script.js
+++ script/script.js
@@ -3,30 +3,48 @@ const textInput = document.querySelector("textarea[name='post_content']")
 const container = document.querySelector(".container");
 let keys = 0
 
-document.addEventListener("DOMContentLoaded",
-  () => {
-    for (let key in localStorage) {
-      const post = document.createElement("div");
-      post.classList.add("Posting");
-      post.classList.add("m-2");
-      post.classList.add("d-flex");
-      post.classList.add("card");
-      post.classList.add("card-body");
-      post.classList.add("rounded-2");
-      post.classList.add("p-1");
-      post.innerHTML = `
+
+console.clear();
+
+function getPosts() {
+  fetch("../data/post.json", {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(data => {
+
+      data.map(post => {
+        const cardPost = document.createElement("div");
+        cardPost.classList.add("Posting");
+        cardPost.classList.add("m-2");
+        cardPost.classList.add("d-flex");
+        cardPost.classList.add("card");
+        cardPost.classList.add("card-body");
+        cardPost.classList.add("rounded-2");
+        cardPost.classList.add("p-1");
+
+
+        cardPost.innerHTML = `
      <img class="perfil col medium" src="img/circle-user-solid-full.svg" alt="foto de perfil">
-          <h3 class="col">{nome}</h3>
+          <h3 class="col">${post.nome}</h3>
         </div>
-        <p>${localStorage.getItem(key)}</p>
+        <p>${post}</p>
       
       </div>
 `;
-      container.appendChild(post);
-    }
+        container.appendChild(cardPost);
+      })
+
+    })
+    .catch((err) => { console.error(err) })
+}
+document.addEventListener("DOMContentLoaded",
+  () => {
+    getPosts();
+
   }
 )
-  
+
 
 inputPost.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -48,9 +66,9 @@ inputPost.addEventListener("submit", (e) => {
       
       </div>
 `;
-textInput.value = " ";
-textInput.focus();
-container.appendChild(post);
-localStorage.setItem(keys, postContent)
-keys++;
+  textInput.value = " ";
+  textInput.focus();
+  container.appendChild(post);
+  localStorage.setItem(keys, postContent)
+  keys++;
 })
